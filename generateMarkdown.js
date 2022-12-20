502ef523ac781c922130da45e80117a4b3b51dff
fabien moreno <fabienmoreno1331@yahoo.com>
2022-12-20 00:56:52 -0600

    index.js finished

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge = '';
+  if (license === 'MIT') {
+    licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)'
+  }
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,10 +18,46 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+function turd(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ##Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Technologies](#technologies)
+  - [Credits](#credits)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [License](#license)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Technologies
+ - ${data.technologies1}
+ - ${data.technologies2}
+ - ${data.technologies3}
+
+ ## Credits
+ ${data.contributing}
+
+ ## Tests
+ ${data.tests}
+
+ ## Questions
+ ${data.questions}
+
+ ## License
+ ${data.license}
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = turd;
--- index.js
+++ index.js
@@ -1,13 +1,164 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of this project?',
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'You must pick a title for this project!'
+            }
+        },
+    },
+    { 
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project.',
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'Please enter a description for your project!'
+            }
+        },
+
+    },
+    { 
+        type: 'input',
+        name: 'installation',
+        message: 'What installation is required for your project?',
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'You must write something or put N/A!'
+            }
+        },
+
+    },
+    { 
+        type: 'input',
+        name: 'usage',
+        message: 'Explain how your project is meant to be utilized.',
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'Please provide usage information!'
+            }
+        },
+
+    },
+    { 
+        type: 'list',
+        name: 'license',
+        message: 'Pick a license for your project.',
+        choices: ['MIT', 'Apache 2.0', 'PDDL'],
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'Please provide usage information!'
+            }
+        },
+
+    },
+    { 
+        type: 'input',
+        name: 'contributing',
+        message: 'Who contributed to this project?',
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'If no contributions were made please enter N/A!'
+            }
+        },
+
+    },
+    { 
+        type: 'input',
+        name: 'questions',
+        message: 'Provide your GitHub username',
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'GitHub username is REQUIRED!'
+            }
+        },
+
+    },
+    { 
+        type: 'input',
+        name: 'tests',
+        message: 'What test methods can a user perform on this project?',
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'Must provide at least ONE test for the users!'
+            }
+        },
+
+    },
+    { 
+        type: 'list',
+        name: 'technologies1',
+        message: 'What is the first language used to build this project? (select one)',
+        choices: ['HTML', 'CSS', 'JavaScript', 'JQuery', 'Node', 'N/A'],
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'You must pick ONE from the list!'
+            }
+        },
+
+    },
+    { 
+        type: 'list',
+        name: 'technologies2',
+        message: 'What is the second language used to build this project? (select one)',
+        choices: ['HTML', 'CSS', 'JavaScript', 'JQuery', 'Node', 'N/A'],
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'You must pick ONE from the list!'
+            }
+        },
+
+    },
+    { 
+        type: 'list',
+        name: 'technologies3',
+        message: 'What is the third language used to build this project? (select one)',
+        choices: ['HTML', 'CSS', 'JavaScript', 'JQuery', 'Node', 'N/A'],
+        validate: (value) => {
+            if(value) {
+                return true
+            }else {
+                return 'You must pick ONE from the list!'
+            }
+        },
+
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer.prompt(questions).then(answers => {
+    const finalMarkdown = generateMarkdown(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
+    fs.writeFile('README.md', finalMarkdown, (error) =>
+    error ? console.log(error) : console.log('Success! You have created a quality README.md file') )
 
-// Function call to initialize app
-init();
+});
+
